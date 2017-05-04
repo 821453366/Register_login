604ad385e416b45bdf92b2408e1452bb67fec982
821453366 <821453366@qq.com>
2017-05-04 14:22:47 +0800

    案例项目初始化

---
+++ src/com/huan/dao/IAdminDao.java
@@ -0,0 +1,7 @@
+package com.huan.dao;
+
+/**数据访问层
+ * Created by pc on 17-5-4.
+ */
+public interface IAdminDao {
+}
---
+++ src/com/huan/entity/Admin.java
@@ -0,0 +1,34 @@
+package com.huan.entity;
+
+/**管理员模块 javabean设计
+ * Created by pc on 17-5-4.
+ */
+public class Admin {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
